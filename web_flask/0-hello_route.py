4f6afac17a1fca1f1675a17ad410c66d18e92cde
Favvie <favourprecious3@gmail.com>
2022-10-13 20:30:51 +0100

    add web flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a flask module tht serves on /
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.get("/", strict_slashes=False)
+def hello_route():
+    return 'Hello HBNB!'
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
