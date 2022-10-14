9fa2ac0d47c012190d83b6f4bd16bccf0fe57804
Favvie <favourprecious3@gmail.com>
2022-10-14 07:06:07 +0100

    add template for 6, and check if number is even or odd

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+add default text to url
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    return 'Hello HBNB'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_text(text):
+    return "C %s" % text.replace("_", " ")
+
+
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route('/python/<text>')
+def hello_default(text):
+    return "Python %s" % text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def hello_int(n):
+    return "%d is a number" % n
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def hello_template(n):
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def hello_odd(n):
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.01", port=5000)
